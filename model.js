5c4a85666de1a6be59f73b6b04b2b93039bab82a
anestisal10 <anestisal10@gmail.com>
2025-08-23 15:59:43 +0300

    Update model.js
    updated deepseek url

--- model.js
+++ model.js
@@ -48,7 +48,7 @@ const models = [
         id: 'deepseek',
         name: 'DeepSeek',
         company: 'DeepSeek AI',
-        url: 'https://www.deepseek.com/chat',
+        url: 'https://chat.deepseek.com/',
         info_url: 'https://martinfowler.com/articles/deepseek-papers.html',
         color: 'indigo',
         icon: 'M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z',
@@ -132,4 +132,5 @@ const patchNotes = [
         link: 'https://qwenlm.github.io/blog/qwen1.5/'
     }
     // Add more notes as needed
+
 ];
